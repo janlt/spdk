3ee90f536bae5622f57d8955bb6b8d3ce6c4bbca
jan.lisowiec@intel.com <jan.lisowiec@intel.com>
2020-04-07 03:19:05 +0200

    Add fileio

---
+++ test/bdev/bdevc++/fileio/FileEmu.cpp
@@ -0,0 +1,185 @@
+/**
+ *  Copyright (c) 2019 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+#include <stdio.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include <string>
+#include <atomic>
+#include <cstdint>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+#include <boost/asio.hpp>
+#include <boost/filesystem.hpp>
+
+#include <string>
+#include <atomic>
+#include <cstdint>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <condition_variable>
+
+#include "FileEmu.h"
+
+using namespace std;
+
+namespace BdevCpp {
+
+FileEmu::FileEmu(const char *_name, int _flags, mode_t _mode, uint64_t _size)
+    : name(_name), flags(_flags), mode(_mode), desc(-1), fd(-1), size(_size), fileSlot(-1) {
+    fd = ::open(_name, flags, mode);
+    if (fd >= 0)
+        desc = fd;
+}
+
+FileEmu::~FileEmu() {
+}
+
+
+int FileEmu::lseek(off_t off, int whence) {
+    switch (whence) {
+    case SEEK_SET: // absolute pos
+        return adjustPos(static_cast<int64_t>(off) - pos.pos);
+        break;
+    case SEEK_CUR: // from current pos
+        return adjustPos(static_cast<int64_t>(off));
+        break;
+    case SEEK_END: // end of file
+        return adjustPos(static_cast<int64_t>(size));
+        break;
+    default:
+        return -1;
+        break;
+    }
+}
+
+int FileEmu::adjustPos(int64_t delta) {
+    if (delta < 0 && static_cast<uint64_t>(abs(delta)) > pos.pos)
+        return -1;
+    uint64_t deltaLbas = delta/geom.optLbaSize + 1;
+    if (pos.posLba + deltaLbas > geom.endLba) {
+        pos.posLun++;
+        pos.posLun %= geom.numLuns;
+        pos.posLba = geom.startLba;
+    } else {
+        pos.posLba += deltaLbas;
+    }
+    return 0;
+}
+
+void FileEmu::setGeom(uint64_t _startLba, uint64_t _endLba, uint8_t _startLun, uint8_t _numLuns) {
+    geom.startLba = _startLba;
+    geom.endLba = _endLba;
+    geom.startLun = _startLun;
+    geom.numLuns = _numLuns;
+}
+
+
+FileMap *FileMap::map = 0;
+std::mutex FileMap::instanceMutex;
+
+FileMap::FileMap(size_t size)
+    : bottomFreeSlot(-1), topFreeSlot(0), startLun(0) {
+    files.resize(size);
+    ::memset(fileMap, '\0', sizeof(fileMap));
+}
+
+FileMap::~FileMap() {
+}
+
+FileEmu *FileMap::getFile(int desc) {
+    if (desc < 0 || static_cast<size_t>(desc) >= files.size())
+        return 0;
+    unique_lock<Lock> w_lock(opMutex);
+    return files[desc].get();
+}
+
+void FileMap::setBottomSlot(uint32_t start) {
+    for (uint32_t i = start ; i < maxFiles ; i++)
+        if (!fileMap[i]) {
+            bottomFreeSlot = i;
+            return;
+        }
+
+    bottomFreeSlot = -1;
+}
+
+int FileMap::putFile(FileEmu *fileEmu, uint64_t numBlk, uint32_t numLun) {
+    if (!fileEmu || fileEmu->desc < 0 || files[fileEmu->desc])
+        return -1;
+
+    unique_lock<Lock> w_lock(opMutex);
+
+    uint32_t freeSlot;
+    if (bottomFreeSlot != static_cast<uint32_t>(-1)) {
+        freeSlot = bottomFreeSlot;
+        fileMap[bottomFreeSlot] = 1;
+        setBottomSlot(bottomFreeSlot);
+    } else {
+        if (topFreeSlot >= maxFiles)
+            return -1;
+        freeSlot = topFreeSlot;
+        fileMap[topFreeSlot] = 1;
+        topFreeSlot++;
+    }
+    fileEmu->geom.startLba = numBlk/maxFiles * freeSlot;
+    fileEmu->geom.endLba = numBlk/maxFiles * (freeSlot + 1);
+    fileEmu->geom.numLuns = numLun;
+    fileEmu->geom.startLun = startLun%numLun;
+    startLun++;
+    fileEmu->fileSlot = freeSlot;
+
+    files[fileEmu->desc].reset(fileEmu);
+
+    return fileEmu->desc;
+}
+
+int FileMap::closeFile(int desc) {
+    if (desc < 0 || static_cast<size_t>(desc) >= files.size())
+        return 0;
+
+    unique_lock<Lock> w_lock(opMutex);
+
+    FileEmu *femu = files[desc].get();
+    fileMap[femu->fileSlot] = 0;
+    if (topFreeSlot - 1 == femu->fileSlot)
+        topFreeSlot = femu->fileSlot;
+    else
+        setBottomSlot(0);
+
+    files[desc].reset();
+    return 0;
+}
+
+FileMap &FileMap::getInstance() {
+    if (!map) {
+        unique_lock<Lock> w_lock(instanceMutex);
+        if (!map)
+            map = new FileMap();
+    }
+    return *map;
+}
+
+} // namespace BdevCpp
---
+++ test/bdev/bdevc++/fileio/FileEmu.h
@@ -0,0 +1,83 @@
+/**
+ *  Copyright (c) 2019 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+#pragma once
+
+namespace BdevCpp {
+
+typedef std::mutex Lock;
+
+struct FileGeom {
+    uint64_t startLba = 0;
+    uint64_t endLba = 0;
+    uint8_t startLun = 0;
+    uint8_t numLuns = 0;
+    uint64_t optLbaSize = 4096;
+};
+
+struct FilePos {
+    uint64_t pos = -1; // absolute pos
+    uint64_t posLba = -1; // Lba for pos within lun
+    uint8_t posLun = -1; // which lun pos is on
+};
+
+class FileEmu {
+  public:
+    FileEmu(const char *_name, int _flags, mode_t _mode, uint64_t _size = 0);
+    virtual ~FileEmu();
+
+    int lseek(off_t off, int whence);
+    int adjustPos(int64_t delta);
+    void setGeom(uint64_t _startLba, uint64_t _endLba, uint8_t _startLun, uint8_t _numLuns);
+
+    std::string name;
+    int flags;
+    mode_t mode;
+    int desc;
+    int fd; // real one
+    uint64_t size;
+    FilePos pos;
+    FileGeom geom;
+    uint32_t fileSlot;
+};
+
+class FileMap {
+  private:
+    FileMap(size_t size = (1 << 16));
+    virtual ~FileMap();
+
+  public:
+    FileEmu *getFile(int desc);
+    int putFile(FileEmu *fileEmu, uint64_t numBlk, uint32_t numLun);
+    void setBottomSlot(uint32_t start);
+    int closeFile(int desc);
+
+    static FileMap &getInstance();
+
+  private:
+    std::vector<std::shared_ptr<FileEmu>> files;
+    static const uint32_t maxFiles = 256;
+    uint8_t fileMap[maxFiles];
+    uint32_t bottomFreeSlot;
+    uint32_t topFreeSlot;
+    uint32_t startLun;
+    std::mutex opMutex;
+
+    static FileMap *map;
+    static std::mutex instanceMutex;
+};
+
+} // namespace BdevCpp
